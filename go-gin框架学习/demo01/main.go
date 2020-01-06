ef8d48f1d024551fb6833f63c34a1327b30c22c8
Mark <jiannma@gmail.com>
2020-01-06 14:59:21 +0800

    增加gin demo01例子

---
+++ go-gin框架学习/demo01/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"apiserver/router"
+	"errors"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	//create the gin engine
+	g := gin.New()
+
+	//gin middlewares
+	middlewares := []gin.HandlerFunc{}
+
+	//routes
+	router.Load(
+		g,
+		middlewares...,
+	)
+
+	go func() {
+		if err := pingServer(); err != nil {
+			log.Fatal("The router has no response, or it might took too long to start up.", err)
+		}
+		log.Print("The router has been deployed successfully.")
+	}()
+
+	log.Printf("Start to listening the incoming requests on http address: %s", ":8080")
+	log.Printf(http.ListenAndServe(":8080", g).Error())
+}
+
+// pingServer pings the http server to make sure the router is working.
+func pingServer() error {
+	for i := 0; i < 2; i++ {
+		// Ping the server by sending a GET request to `/health`.
+		resp, err := http.Get("http://127.0.0.1:8080" + "/api/health")
+		if err == nil && resp.StatusCode == 200 {
+			return nil
+		}
+
+		// Sleep for a second to continue the next ping.
+		log.Print("Waiting for the router, retry in 1 second.")
+		time.Sleep(time.Second)
+	}
+	return errors.New("Cannot connect to the router.")
+}
